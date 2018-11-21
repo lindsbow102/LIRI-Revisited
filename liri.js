da2740b63ee58b4cb08046af693ce76a379e3c3b
Lindsey Bowen <lindsbow102@yahoo.com>
2018-11-21 15:11:11 -0700

    Spotify working, new spotify package

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 const keys = require("./keys");
-
 const Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 
 const getMyTweets = () => {
   const client = new Twitter(keys.twitterKeys);
@@ -22,16 +22,48 @@ const getMyTweets = () => {
   });
 };
 
-const pick = ((caseData, functionData) => {
-    switch(caseData) {
-        case 'my-tweets' :
-            getMyTweets();
-            break;
-        default: 
-            console.log('LIRI does not know that!');
-    }
+const getArtistNames = ((artist) => {
+    return artist.name;
 });
 
-const runThis = ((argOne, argTwo) => {
-    pick(argOne, argTwo);
-});
+const getMeSpotify = songName => {
+  const spotify = new Spotify(keys.spotify);
+
+  spotify.search({ type: "track", query: songName }, function(
+    err,
+    data
+  ) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    const songs = data.tracks.items;
+    for (let i = 0; i < songs.length; i++) {
+        console.log(i);
+        console.log("Artist(s): " + songs[i].artists.map(getArtistNames));
+        console.log("Song name: " + songs[i].name);
+        console.log("Preview song: " + songs[i].preview_url);
+        console.log("Album: " + songs[i].album.name);
+        console.log("----------------------------");
+    }
+  });
+};
+
+const pick = (caseData, functionData) => {
+  switch (caseData) {
+    case "my-tweets":
+      getMyTweets();
+      break;
+    case "spotify-this-song" :
+        getMeSpotify(functionData);
+        break;
+    default:
+      console.log("LIRI does not know that!");
+  }
+};
+
+const runThis = (argOne, argTwo) => {
+  pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
